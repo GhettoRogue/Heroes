49c42fb948a8e32bdfd6863bc165765715325ef1
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-07-21 21:13:57 +0400

    update add ConfigGame.h

---
+++ lib/ConfigGame.h
@@ -0,0 +1,48 @@
+#pragma once
+
+class ConfigGame {
+private:
+    static int _defenceLightArmor;
+    static int _defenceHeavyArmor;
+    static int _healthZerg;
+    static int _damageZerg;
+    static int _healthProtos;
+    static int _damageProtos;
+
+public:
+    static void Init() {
+        _defenceLightArmor = 5;
+        _defenceHeavyArmor = 10;
+        _healthZerg = 100;
+        _damageZerg = 50;
+        _healthProtos = 150;
+        _damageProtos = 75;
+    }
+
+    static int getDefenceLightArmor() {
+        return _defenceLightArmor;
+    }
+
+    static int getDefenceHeavyArmor() {
+        return _defenceHeavyArmor;
+    }
+
+    static int getHealthZerg() {
+        return _healthZerg;
+    }
+
+    static int getDamageZerg() {
+        return _damageZerg;
+    }
+
+    static int getHealthProtos() {
+        return _healthProtos;
+    }
+
+    static int getDamageProtos() {
+        return _damageProtos;
+    }
+};
+
+
+
